996012db9b4c6fb6cf9f39f24a940c63ee4db066
ylyutskanov <yordan.lyutskanov@dreamix.eu>
2020-12-10 07:34:11 +0200

    Closes #1 -- Initial Spring Boot project created

---
+++ src/main/java/eu/dreamix/bpmgateway/BpmGatewayApplication.java
@@ -0,0 +1,13 @@
+package eu.dreamix.bpmgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpmGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/dreamix/bpmgateway/BpmGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.dreamix.bpmgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpmGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
